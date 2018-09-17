8c47d747ea240151e6758fbd9ca87105583dc183
butlerwillkillu <42875221+butlerwillkillu@users.noreply.github.com>
2018-09-17 15:04:19 -0400

    Initial commit

---
+++ src/butlercollen/llcs/main/Main.java
@@ -0,0 +1,27 @@
+package butlercollen.llcs.main;
+
+public class Main
+	{
+
+		public static void main(String[] args)
+			{
+				String wasupp = "wasupp!";
+				
+				System.out.println(wasupp);
+				
+				int numberOne = 5;
+						
+				System.out.println(numberOne);
+				
+				double myLife = .00;
+						
+				System.out.println(myLife);
+				
+				boolean amICool = false;
+				
+				System.out.println(amICool);
+
+
+			}
+
+	}
